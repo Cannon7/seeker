293d4db5faa399793b1d0c8b6972127e90a85526
Cannon7 <roa22002@byui.edu>
2022-11-29 13:32:14 -0700

    GG

---
+++ Sollution.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Seeker;
+class Sollution
+{
+    Random rnd = new Random();
+    public int location();
+    {
+        private int Soll = rnd.Next(1000);
+        return Soll
+    }
+}
---
+++ director.cs
@@ -0,0 +1,6 @@
+using System;
+namespace Seeker;
+class Director
+{
+    
+}
